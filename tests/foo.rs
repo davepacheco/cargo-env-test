74dccf4c920bad183091e0b33bac4845ac46d3d8
David Pacheco <dap@oxidecomputer.com>
2020-08-06 12:42:34 -0700

    it is only available at compile time

--- tests/foo.rs
+++ tests/foo.rs
@@ -1,6 +1,8 @@
 #[test]
 fn the_test()
 {
+    let value = env!("CARGO_BIN_EXE_cargo-env");
+    println!("dap: {}", value);
     for (key, value) in std::env::vars() {
         println!("{}: {}", key, value);
     }
