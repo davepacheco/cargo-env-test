932ef508703cb2092d966208584ec36c48dd920e
David Pacheco <dap@oxidecomputer.com>
2020-08-06 15:21:41 -0700

    add a second binary

--- tests/foo.rs
+++ tests/foo.rs
@@ -3,6 +3,9 @@ fn the_test()
 {
     let value = env!("CARGO_BIN_EXE_cargo-env");
     println!("compile-time: CARGO_BIN_EXE_cargo-env: {}", value);
+    let value = env!("CARGO_BIN_EXE_cargo-env2");
+    println!("compile-time: CARGO_BIN_EXE_cargo-env2: {}", value);
+
     for (key, value) in std::env::vars() {
         if key.starts_with("CARGO_") {
             println!("runtime: {}: {}", key, value);
